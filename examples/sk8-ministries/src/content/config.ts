8d2869031c22d86ed5f3f8b4b749bc5b43ce82bc
Simon <lgtnt@protonmail.com>
2024-07-05 16:24:19 +0200

    reset config ts to non dynamic import

--- examples/sk8-ministries/src/content/config.ts
+++ examples/sk8-ministries/src/content/config.ts
@@ -2,6 +2,6 @@ import { mediaSchema, mediaTypeSchema } from "@lightnet/library/content"
 import { defineCollection } from "astro:content"
 
 export const collections = {
-  media: defineCollection({ type: "data", schema: await mediaSchema() }),
+  media: defineCollection({ type: "data", schema: mediaSchema }),
   "media-types": defineCollection({ type: "data", schema: mediaTypeSchema }),
 }
