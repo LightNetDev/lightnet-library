717f292eff447ef74712118e98a5e45511e6ae42
Simon <lgtnt@protonmail.com>
2024-07-09 13:46:07 +0200

    improve hero

--- packages/library/exports/components.ts
+++ packages/library/exports/components.ts
@@ -1,5 +1,5 @@
 export { default as Gallery } from "../src/components/Gallery.astro"
-export { default as HeroSection } from "../src/components/HeroSection.astro"
+export { default as Hero } from "../src/components/Hero.astro"
 export { default as Icon } from "../src/components/Icon"
 export { default as ImageSection } from "../src/components/ImageSection.astro"
 export { default as Section } from "../src/components/Section.astro"
