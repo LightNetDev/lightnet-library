0b98b151d2f23cee64a12ac3efee480255700de9
Simon <lgtnt@protonmail.com>
2024-10-08 10:04:50 +0200

    removes mediaQuery (was not being used)

--- packages/library/src/content/get-media-items.ts
+++ packages/library/src/content/get-media-items.ts
@@ -2,18 +2,14 @@ import { getCollection, getEntry } from "astro:content"
 
 import { verifySchemaAsync } from "../utils/verify-schema"
 import { mediaItemEntrySchema } from "./content-schema-internal"
-import { type MediaQuery, mediaQuery } from "./media-query"
 
 export const getMediaItem = async (id: string) => {
   const item = await getEntry("media", id)
   return prepareItem(item)
 }
 
-export const getMediaItems = async (query?: MediaQuery) => {
-  const items: unknown[] = await getCollection(
-    "media",
-    query && mediaQuery(query),
-  )
+export const getMediaItems = async () => {
+  const items: unknown[] = await getCollection("media")
   return Promise.all(items.map(prepareItem))
 }
 
