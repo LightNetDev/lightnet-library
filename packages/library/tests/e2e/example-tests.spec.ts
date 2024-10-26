ce947b12db08389b89461b8110d51c27d5d83cde
Ivana Jones <54974353+ivasve@users.noreply.github.com>
2024-10-26 19:24:28 +1100

    add test

---
+++ packages/library/tests/e2e/example-tests.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect, type Page } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4322/")
+})
+
+test('Verify page title', async ({ page }) => {
+  const title = await page.title();
+  expect(title).toBe('Sk8 Ministries');
+});
+
+const switchLanguage = async (page: Page, language: string) => {
+  // TODO capturing the locator doesn't work properly, adding dataId didn't help
+  await page.getByLabel('Select language').click();
+  await page.getByRole('link', { name: language }).click();
+  await page.waitForLoadState('load');
+};
+
+test('Verify language mutation', async ({ page }) => {
+  expect(page.url().endsWith(`/`)).toBe(true);
+
+  await switchLanguage(page, 'Deutsch');
+  console.log('Current URL after selecting Deutsch:', page.url());
+  expect(page.url().endsWith(`/de/`)).toBe(true);
+
+  await switchLanguage(page, 'English');
+  console.log(page.url())
+  expect(page.url().endsWith(`/en/`)).toBe(true);
+});
