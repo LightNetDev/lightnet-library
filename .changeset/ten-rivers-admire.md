4e4b6e356400ce548e014ee824e87845651bc9f8
Simon <51213509+si-fab@users.noreply.github.com>
2024-12-03 14:51:24 +0100

    implement feedback from details page review (#147)

---
+++ .changeset/ten-rivers-admire.md
@@ -0,0 +1,5 @@
+---
+"@lightnet/library": patch
+---
+
+Improved details page layout/behavior.
--- packages/library/src/pages/details-page/utils/create-content-metadata.ts
+++ packages/library/src/pages/details-page/utils/create-content-metadata.ts
@@ -1,34 +1,46 @@
 import { isExternalUrl } from "../../../utils/urls"
 
-const BROWSER_SUPPORTED_TYPES = new Set([
-  // web
-  "html",
-  "htm",
-  "json",
-  "xml",
-  "svg",
-  "php",
-  // image
-  "jpg",
-  "jpeg",
-  "png",
-  "gif",
-  "ico",
-  "webp",
-  "svg",
-  // audio
-  "mp3",
-  "wav",
-  "aac",
-  "ogg",
-  // video
-  "mp4",
-  "webm",
-  "ogv",
-  // text
-  "pdf",
-  "txt",
-])
+export type UrlType =
+  | "link"
+  | "source"
+  | "image"
+  | "audio"
+  | "video"
+  | "text"
+  | "package"
+
+const KNOWN_EXTENSIONS: Record<
+  string,
+  { type: UrlType; canBeOpened?: boolean } | undefined
+> = {
+  htm: { type: "link", canBeOpened: true },
+  html: { type: "link", canBeOpened: true },
+  php: { type: "link", canBeOpened: true },
+  json: { type: "source", canBeOpened: true },
+  xml: { type: "source", canBeOpened: true },
+  svg: { type: "image", canBeOpened: true },
+  jpg: { type: "image", canBeOpened: true },
+  jpeg: { type: "image", canBeOpened: true },
+  png: { type: "image", canBeOpened: true },
+  gif: { type: "image", canBeOpened: true },
+  ico: { type: "image", canBeOpened: true },
+  webp: { type: "image", canBeOpened: true },
+  mp3: { type: "audio", canBeOpened: true },
+  wav: { type: "audio", canBeOpened: true },
+  aac: { type: "audio", canBeOpened: true },
+  ogg: { type: "audio", canBeOpened: true },
+  mp4: { type: "video", canBeOpened: true },
+  webm: { type: "video", canBeOpened: true },
+  ogv: { type: "video", canBeOpened: true },
+  pdf: { type: "text", canBeOpened: true },
+  txt: { type: "text", canBeOpened: true },
+  epub: { type: "text" },
+  zip: { type: "package" },
+  ppt: { type: "text" },
+  pptx: { type: "text" },
+  doc: { type: "text" },
+  docx: { type: "text" },
+} as const
 
 export function createContentMetadata({
   url,
@@ -41,7 +53,7 @@ export function createContentMetadata({
   const hasExtension = lastUrlSegment.includes(".")
   const extension = hasExtension
     ? lastUrlSegment.split(".").slice(-1)[0].toLowerCase()
-    : "link"
+    : ""
 
   const isExternal = isExternalUrl(url)
 
@@ -50,11 +62,9 @@ export function createContentMetadata({
     ? lastUrlSegment.slice(0, -(extension.length + 1))
     : undefined
   const name = customName ?? fileName ?? linkName
-
-  const canBeOpened = !hasExtension || BROWSER_SUPPORTED_TYPES.has(extension)
-  const canBeDownloaded =
-    (!isExternal && hasExtension) ||
-    (isExternal && hasExtension && !BROWSER_SUPPORTED_TYPES.has(extension))
+  const type = KNOWN_EXTENSIONS[extension]?.type ?? "link"
+  const canBeOpened =
+    !hasExtension || !!KNOWN_EXTENSIONS[extension]?.canBeOpened
 
   return {
     url,
@@ -62,7 +72,7 @@ export function createContentMetadata({
     isExternal,
     name,
     canBeOpened,
-    canBeDownloaded,
+    type,
     target: isExternal ? "_blank" : "_self",
-  }
+  } as const
 }
