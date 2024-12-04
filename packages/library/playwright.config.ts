d861ef4e5e4a2bed5057a7a472b34bab2554f854
Simon <51213509+si-fab@users.noreply.github.com>
2024-12-04 16:57:32 +0100

    Do not run e2e tests in parallel (#156)

--- packages/library/playwright.config.ts
+++ packages/library/playwright.config.ts
@@ -5,14 +5,12 @@ import { defineConfig, devices } from "@playwright/test"
  */
 export default defineConfig({
   testDir: "./__e2e__",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests. */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
