e10568f20bdbcaf486eb62116b96e79896325e89
Ivana Jones <54974353+ivasve@users.noreply.github.com>
2024-12-06 10:46:33 +1100

    run pnpm format for the project

--- packages/library/__e2e__/fixtures/basics/astro.config.mjs
+++ packages/library/__e2e__/fixtures/basics/astro.config.mjs
@@ -2,7 +2,7 @@ import lightnetLibrary from "@lightnet/library"
 import { defineConfig } from "astro/config"
 
 import { defaultLocale, languages } from "./src/i18n"
-import de from "./src/translations/de.json";
+import de from "./src/translations/de.json"
 import en from "./src/translations/en.json"
 
 // https://astro.build/config
