376c4ee2b362a36053457498afaa2e4514f013e7
Simon <lgtnt@protonmail.com>
2025-01-22 07:44:48 +0100

    Add option to flip an icon.

---
+++ .changeset/early-kings-help.md
@@ -0,0 +1,5 @@
+---
+"@lightnet/library": patch
+---
+
+Adds option to flip an icon.
--- packages/library/src/components/Icon.tsx
+++ packages/library/src/components/Icon.tsx
@@ -1,20 +1,24 @@
 /**
  * Icon Component.
  *
- * @param class containing a material design icon name prefixed with 'mdi--'
+ * @param className containing a material design icon name prefixed with 'mdi--'
+ * @param ariaLabel accessibility label to be added e.g. for people using a screen reader.
+ * @param flipIcon if set to true this will mirror the icon along its x-axis. Useful for RTL layouts.
  * @see https://pictogrammers.com/library/mdi/ for available icons
  * @returns icon
  */
 export default function Icon({
   className,
   ariaLabel,
+  flipIcon,
 }: {
   className?: string
   ariaLabel?: string
+  flipIcon?: boolean
 }) {
   return (
     <span
-      className={`iconify text-2xl ${className ?? ""}`}
+      className={`iconify text-2xl ${className ?? ""} ${flipIcon && "scale-x-[-1]"}`}
       aria-label={ariaLabel}
     />
   )
